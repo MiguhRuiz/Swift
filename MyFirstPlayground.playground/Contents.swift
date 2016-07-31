b8605e36b9656b7a62efb15bd75f687089ed282b
Miguh Ruiz <miguhruiz@gmail.com>
2016-07-31 18:40:04 +0200

    Add things to the playground

--- MyFirstPlayground.playground/Contents.swift
+++ MyFirstPlayground.playground/Contents.swift
@@ -3,3 +3,29 @@
 import UIKit
 
 var str = "Hello, playground"
+var str2 = "Hello World"
+
+var a = str2.characters.count
+//print(a)
+
+for i in 1...5 {
+    print("\(i) times 5 is equal to.. \(i*5)")
+}
+
+var books = ["JavaScript de Good Parts": "An awesome guy", "Aprendiendo JavaScript": "Carlos Azaustre"]
+
+for (book, author) in books {
+    print("\(book) fue escrito por \(author)")
+}
+
+func saludar() {
+    print("Mi primera función en Swift")
+}
+
+saludar()
+
+func saludopar(n: String) {
+    print("Hola \(n)")
+}
+
+saludopar("Luis")
